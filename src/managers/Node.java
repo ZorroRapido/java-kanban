c8035d028c0c74d0d5a5f187d4076b48a7247ebd
i.s.melnikov <i.s.melnikov@tinkoff.ru>
2022-10-02 03:17:15 +0300

    fix: изменить модификаторы доступа атрибутов класса Node

--- src/managers/Node.java
+++ src/managers/Node.java
@@ -3,9 +3,9 @@ package managers;
 import java.util.Objects;
 
 public class Node<Task> {
-    public Task data;
-    public Node<Task> next;
-    public Node<Task> prev;
+    private Task data;
+    private Node<Task> next;
+    private Node<Task> prev;
 
     public Node(Node<Task> prev, Task data, Node<Task> next) {
         this.prev = prev;
@@ -26,5 +26,29 @@ public class Node<Task> {
     @Override
     public int hashCode() {
         return Objects.hash(data, next, prev);
+    }
+
+    public Task getData() {
+        return data;
+    }
+
+    public void setData(Task data) {
+        this.data = data;
+    }
+
+    public Node<Task> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<Task> next) {
+        this.next = next;
+    }
+
+    public Node<Task> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<Task> prev) {
+        this.prev = prev;
     }
 }
